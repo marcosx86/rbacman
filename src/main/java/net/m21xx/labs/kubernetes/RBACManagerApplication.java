35b6cb5a8c0838deee5bf836defc367d635eb344
Marcos Silva Cunha <marcos21xx@gmail.com>
2022-09-07 23:18:54 -0300

    Switched dependency for Kubernetes API and now a better fetching of resources

--- src/main/java/net/m21xx/labs/kubernetes/RBACManagerApplication.java
+++ src/main/java/net/m21xx/labs/kubernetes/RBACManagerApplication.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"net.m21xx"})
 public class RBACManagerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
--- src/main/java/net/m21xx/labs/kubernetes/service/impl/KubernetesServiceImpl.java
+++ src/main/java/net/m21xx/labs/kubernetes/service/impl/KubernetesServiceImpl.java
@@ -1,12 +1,9 @@
 package net.m21xx.labs.kubernetes.service.impl;
 
-import io.kubernetes.client.openapi.ApiClient;
-import io.kubernetes.client.openapi.ApiException;
-import io.kubernetes.client.openapi.Configuration;
-import io.kubernetes.client.openapi.apis.CoreV1Api;
-import io.kubernetes.client.openapi.models.V1NamespaceList;
-import io.kubernetes.client.util.Config;
-import net.m21xx.labs.kubernetes.model.KubeApiResponse;
+import io.fabric8.kubernetes.api.model.APIResourceList;
+import io.fabric8.kubernetes.api.model.GroupVersionForDiscovery;
+import io.fabric8.kubernetes.client.*;
+import lombok.extern.log4j.Log4j2;
 import net.m21xx.labs.kubernetes.service.KubernetesService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,10 +11,11 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
-
-import static net.m21xx.labs.kubernetes.util.Util.readValueFrom;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@Log4j2
 public class KubernetesServiceImpl implements KubernetesService {
 
     @Value("${kubernetes.api.url:https://kubernetes.default.svc}")
@@ -29,114 +27,58 @@ public class KubernetesServiceImpl implements KubernetesService {
     @Value("${kubernetes.api.validateSSL:false}")
     private  boolean kubernetesValidateSSL;
 
-    private ApiClient client;
+    private KubernetesClient client;
 
     @PostConstruct
     private void setup() {
 
-        client = Config.fromToken(kubernetesUrl, kubernetesToken, kubernetesValidateSSL);
-        Configuration.setDefaultApiClient(client);
+        Config config = new ConfigBuilder()
+                .withMasterUrl(kubernetesUrl)
+                .withTrustCerts(! kubernetesValidateSSL)
+                .withOauthToken(kubernetesToken)
+                .build();
+        client = new KubernetesClientBuilder()
+                .withConfig(config)
+                .build();
     }
 
     @Override
     public List<String> getAllResources() {
 
-        try {
-            List<String> lst = new ArrayList<>();
+        List<String> groups = client.getApiGroups().getGroups().stream()
+                .map(r -> r.getVersions().stream()
+                        .map(GroupVersionForDiscovery::getGroupVersion)
+                        .collect(Collectors.toList()))
+                .reduce(new ArrayList<>(), (lst, itm) -> {
+                    lst.addAll(itm);
+                    return lst;
+                });
+        groups.add(0, "v1");
+        groups.forEach(System.out::println);
 
-            CoreV1Api coreV1 = new CoreV1Api();
-            V1NamespaceList lst1 = coreV1.listNamespace(null, null, null, null,
-                    null, null, null, null,
-                    null, null);
-
-//            List<String> lst = lst1.getItems().stream()
-//                    .map(res -> res.getMetadata().getName())
-//                    .collect(Collectors.toList());
-
-//            CoreV1Api coreV1 = new CoreV1Api();
-//            V1APIResourceList coreResources = coreV1.getAPIResources();
-//            lst.add(String.format("\ngroupVersion: %s\n", coreResources.getGroupVersion()));
-//            coreResources.getResources().stream()
-//                    .map(res -> {
-//                        String verbs = res.getVerbs().stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        String categories = Optional.ofNullable(res.getCategories())
-//                                .orElse(List.of(""))
-//                                .stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        return String.format("(core) %s -> %s: %s (%s)", res.getVersion(), res.getName(), verbs, categories);
-//                    })
-//                    .forEach(lst::add);
-//
-//            RbacAuthorizationV1Api rbacV1 = new RbacAuthorizationV1Api();
-//            V1APIResourceList rbacResources = rbacV1.getAPIResources();
-//            lst.add(String.format("\ngroupVersion: %s\n", rbacResources.getGroupVersion()));
-//            rbacResources.getResources().stream()
-//                    .map(res -> {
-//                        String verbs = res.getVerbs().stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        String categories = Optional.ofNullable(res.getCategories())
-//                                .orElse(List.of(""))
-//                                .stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        return String.format("(core) %s -> %s: %s (%s)", res.getVersion(), res.getName(), verbs, categories);
-//                    })
-//                    .forEach(lst::add);
-//
-//            NetworkingV1Api netV1 = new NetworkingV1Api();
-//            V1APIResourceList netResources = netV1.getAPIResources();
-//            lst.add(String.format("\ngroupVersion: %s\n", netResources.getGroupVersion()));
-//            netResources.getResources().stream()
-//                    .map(res -> {
-//                        String verbs = res.getVerbs().stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        String categories = Optional.ofNullable(res.getCategories())
-//                                .orElse(List.of(""))
-//                                .stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        return String.format("(core) %s -> %s: %s (%s)", res.getVersion(), res.getName(), verbs, categories);
-//                    })
-//                    .forEach(lst::add);
-//
-//            ApiregistrationV1Api apiregV1 = new ApiregistrationV1Api();
-//            V1APIResourceList apiregResrouces = apiregV1.getAPIResources();
-//            lst.add(String.format("\ngroupVersion: %s\n", apiregResrouces.getGroupVersion()));
-//            apiregResrouces.getResources().stream()
-//                    .map(res -> {
-//                        String verbs = res.getVerbs().stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        String categories = Optional.ofNullable(res.getCategories())
-//                                .orElse(List.of(""))
-//                                .stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        return String.format("(core) %s -> %s: %s (%s)", res.getVersion(), res.getName(), verbs, categories);
-//                    })
-//                    .forEach(lst::add);
-//
-//            ApiextensionsV1Api apiextV1 = new ApiextensionsV1Api();
-//            V1APIResourceList apiextResrouces = apiextV1.getAPIResources();
-//            lst.add(String.format("\ngroupVersion: %s\n", apiextResrouces.getGroupVersion()));
-//            apiextResrouces.getResources().stream()
-//                    .map(res -> {
-//                        String verbs = res.getVerbs().stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        String categories = Optional.ofNullable(res.getCategories())
-//                                .orElse(List.of(""))
-//                                .stream()
-//                                .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
-//                        return String.format("(core) %s -> %s: %s (%s)", res.getVersion(), res.getName(), verbs, categories);
-//                    })
-//                    .forEach(lst::add);
-
-            return lst;
-        } catch (ApiException e) {
-            KubeApiResponse obj = readValueFrom(e.getResponseBody(), KubeApiResponse.class);
-            System.out.println(obj.getMessage());
-
-            e.printStackTrace();
+        List<String> lst = new ArrayList<>();
+        for (String group : groups) {
+            lst.add(String.format("\n#### ALL RESOURCES FOR %s ####", group));
+            lst.addAll(getApiResourcesFor(group));
         }
 
-        return null;
+        return lst;
+    }
+
+    private List<String> getApiResourcesFor(String groupVersion) {
+        List<String> lst;
+        APIResourceList apiResourceList = client.getApiResources(groupVersion);
+        lst = apiResourceList.getResources().stream()
+                .map(res -> {
+                    String verbs = res.getVerbs().stream()
+                            .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
+                    String categories = Optional.ofNullable(res.getCategories())
+                            .orElse(List.of(""))
+                            .stream()
+                            .reduce("", (a, b) -> "".equals(a) ? b : String.format("%s, %s", a, b));
+                    return String.format("(core) %s -> %s: %s (%s)", res.getVersion(), res.getName(), verbs, categories);
+                }).collect(Collectors.toList());
+        return lst;
     }
 
 }
